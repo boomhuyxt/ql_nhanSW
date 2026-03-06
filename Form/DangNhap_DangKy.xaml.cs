4350168ca7f2ee6aef2a4ab692be32c7356bfeb9
Bell <belldinhnguyen140905@gmail.com>
2026-03-06 10:12:27 +0700

    giao diện form đăng nhập - đăng ký

--- Form/DangNhap_DangKy.xaml.cs
+++ Form/DangNhap_DangKy.xaml.cs
@@ -20,6 +20,27 @@ namespace ql_nhanSW.Form
         public Window1()
         {
             InitializeComponent();
+
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void SwitchRegister_Click(object sender, RoutedEventArgs e)
+        {
+            SignInPanel.Visibility = Visibility.Collapsed;
+            SignUpPanel.Visibility = Visibility.Visible;
+        }
+
+        private void SwitchLogin_Click(object sender, RoutedEventArgs e)
+        {
+            SignInPanel.Visibility = Visibility.Visible;
+            SignUpPanel.Visibility = Visibility.Collapsed;
         }
     }
 }
